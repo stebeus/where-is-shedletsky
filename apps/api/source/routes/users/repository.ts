a265e6c584236bd8afece58ad24da809bd842f1e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-15 13:25:41 -0300

    style(api): rename `user` object to data

--- apps/api/source/routes/users/repository.ts
+++ apps/api/source/routes/users/repository.ts
@@ -10,27 +10,27 @@ import { hash } from './crypto.ts';
 export const create = async ({ username, password, bestTime }: User) => {
 	const hashedPassword = await hash(password);
 
-	const [user] = await database
+	const [data] = await database
 		.insert(users)
 		.values({ username, password: hashedPassword, bestTime })
 		.returning();
 
-	return user;
+	return data;
 };
 
 export const findMany = async () => await database.select().from(users).orderBy(users.bestTime);
 
 export const findFirst = async (username: string) => {
-	const [user] = await database.select().from(users).where(eq(users.username, username)).limit(1);
-	return user;
+	const [data] = await database.select().from(users).where(eq(users.username, username)).limit(1);
+	return data;
 };
 
 export const update = async ({ username, bestTime }: User) => {
-	const [user] = await database
+	const [data] = await database
 		.update(users)
 		.set({ username, bestTime })
 		.where(eq(users.username, username))
 		.returning();
 
-	return user;
+	return data;
 };
