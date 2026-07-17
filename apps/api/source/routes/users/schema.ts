a7df8a330001f5727dc88c4f24786721a35bc058
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-17 17:06:11 -0300

    style(api): improve minimum password length message

--- apps/api/source/routes/users/schema.ts
+++ apps/api/source/routes/users/schema.ts
@@ -27,7 +27,7 @@ export const userSchema = z.object({
 		.max(username.length as number, `Username cannot be longer than ${username.length} characters`)
 		.regex(alphanumericRegex, 'Username must only contain alphanumeric characters')
 		.refine(isUsernameAvailable, 'Username is already taken'),
-	password: z.string().trim().min(8, 'Password must be at least 8 characters long'),
+	password: z.string().trim().min(8, 'Password must have at least 8 characters'),
 	bestTime,
 });
 
