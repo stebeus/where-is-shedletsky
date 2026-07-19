55823b944c52a1cd9ea6804169d7bc63a6e0d7b3
stebeus <189894010+stebeus@users.noreply.github.com>
2026-07-19 13:21:00 -0300

    refactor(api): set status in the `json` method

--- apps/api/source/routes/users/index.ts
+++ apps/api/source/routes/users/index.ts
@@ -11,15 +11,11 @@ users.get('/', async (c) => c.json({ data: await findMany() }));
 users.post('/sign-in', sValidator('form', signinSchema), async (c) => {
 	const form = c.req.valid('form');
 	const data = await update(form);
-
-	c.status(201);
-	return c.json({ data });
+	return c.json({ data }, 201);
 });
 
 users.post('/sign-up', sValidator('form', userSchema), async (c) => {
 	const form = c.req.valid('form');
 	const data = await create(form);
-
-	c.status(201);
-	return c.json({ data });
+	return c.json({ data }, 201);
 });
