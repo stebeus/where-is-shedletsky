ddf0c4b807de0b2378342b7c7416c6597e937576
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-06 16:59:34 -0300

    refactor(api): replace `varchar` type with `text`
    Remove the need to migrate column length changes.

--- apps/api/src/routes/users/schema.ts
+++ apps/api/src/routes/users/schema.ts
@@ -1,15 +1,8 @@
 import { snakeCase } from 'drizzle-orm/pg-core';
 
-import { userSchema } from '@repo/contracts/users';
-
-const { username } = userSchema.shape;
-
 export const users = snakeCase.table('users', (t) => ({
 	id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
-	username: t
-		.varchar({ length: username.maxLength as number })
-		.notNull()
-		.unique(),
+	username: t.text().notNull().unique(),
 	password: t.text().notNull(),
 	bestTime: t.interval({ fields: 'hour to second', precision: 3 }).notNull(),
 	createdAt: t.timestamp({ withTimezone: true }).defaultNow(),
