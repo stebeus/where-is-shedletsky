88976b24af9826ca8d6411b72bd60708850f552c
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-06 16:52:01 -0300

    refactor(api): use `userSchema` username max length

--- apps/api/src/routes/users/schema.ts
+++ apps/api/src/routes/users/schema.ts
@@ -1,8 +1,15 @@
 import { snakeCase } from 'drizzle-orm/pg-core';
 
+import { userSchema } from '@repo/contracts/users';
+
+const { username } = userSchema.shape;
+
 export const users = snakeCase.table('users', (t) => ({
 	id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
-	username: t.varchar({ length: 50 }).notNull().unique(),
+	username: t
+		.varchar({ length: username.maxLength as number })
+		.notNull()
+		.unique(),
 	password: t.text().notNull(),
 	bestTime: t.interval({ fields: 'hour to second', precision: 3 }).notNull(),
 	createdAt: t.timestamp({ withTimezone: true }).defaultNow(),
