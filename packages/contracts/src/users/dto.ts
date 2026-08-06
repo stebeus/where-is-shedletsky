5989a7b4620cd517575bda4dc5b1f99695012fd5
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-06 16:58:34 -0300

    refactor(contracts): move user validations to base schema

--- packages/contracts/src/users/dto.ts
+++ packages/contracts/src/users/dto.ts
@@ -3,17 +3,8 @@ import * as z from 'zod';
 import { newUserSchema, type User } from './entity.js';
 import { formatMilliseconds } from './helpers.js';
 
-const alphanumericRegex = /^\w+$/;
-
 export const authSchema = z.object({
 	...newUserSchema.shape,
-	username: z
-		.string()
-		.trim()
-		.min(1, 'Username is required')
-		.max(50, 'Username cannot be longer than 50 characters')
-		.regex(alphanumericRegex, 'Username must only contain alphanumeric characters'),
-	password: z.string().trim().min(8, 'Password must be at least 8 characters long'),
 	bestTime: z.coerce.number().int().transform(formatMilliseconds),
 });
 
--- packages/contracts/src/users/entity.ts
+++ packages/contracts/src/users/entity.ts
@@ -1,9 +1,16 @@
 import * as z from 'zod';
 
+const alphanumericRegex = /^\w+$/;
+
 export const userSchema = z.object({
 	id: z.int().positive(),
-	username: z.string(),
-	password: z.string(),
+	username: z
+		.string()
+		.trim()
+		.min(1, 'Username is required')
+		.max(50, 'Username cannot be longer than 50 characters')
+		.regex(alphanumericRegex, 'Username must only contain alphanumeric characters'),
+	password: z.string().trim().min(8, 'Password must be at least 8 characters long'),
 	bestTime: z.string(),
 	createdAt: z.date(),
 	updatedAt: z.date(),
