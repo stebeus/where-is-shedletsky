d808fad2e7818034f68d115a7f0274f269413f63
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-08 08:10:31 -0300

    refactor(api): use standard-validator

--- apps/api/src/middleware/validator.ts
+++ apps/api/src/middleware/validator.ts
@@ -1,15 +1,15 @@
 import type { ValidationTargets } from 'hono';
+import type * as z from 'zod';
 
-import { zValidator } from '@hono/zod-validator';
-import * as z from 'zod';
+import { flattenErrors, sValidator } from '@hono/standard-validator';
 
 export const validate = <Target extends keyof ValidationTargets, Schema extends z.ZodType>(
 	target: Target,
 	schema: Schema,
 ) =>
-	zValidator(target, schema, (result, c) => {
+	sValidator(target, schema, (result, c) => {
 		if (!result.success) {
-			const flattenedError = z.flattenError(result.error);
-			return c.json(flattenedError, 400);
+			const flattenedErrors = flattenErrors(result.error);
+			return c.json(flattenedErrors, 400);
 		}
 	});
