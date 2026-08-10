a7f3c06e2b1abd41dc88a576e469adbb9d37aee3
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-10 16:06:03 -0300

    refactor(web): use serialized payload property

--- apps/web/src/utils/fetch.ts
+++ apps/web/src/utils/fetch.ts
@@ -1,27 +1,38 @@
 import { env } from '#env.ts';
 
-export class FetchError extends Error {
+export class FetchError<ErrorResponse = unknown> extends Error {
 	static isFetchError(value: unknown) {
 		return value instanceof FetchError;
 	}
 
-	readonly status;
-	readonly #res;
+	static async from<ErrorResponse>(res: Response) {
+		const { statusText, status } = res;
 
-	constructor(res: Response) {
-		super(res.statusText);
-		this.status = res.status;
-		this.#res = res;
+		try {
+			const payload = await res.json();
+			return new FetchError<ErrorResponse>(statusText, status, payload);
+		} catch {
+			const message = await res.text();
+			return new FetchError<ErrorResponse>(message, status);
+		}
 	}
 
-	async toJson<ErrorResponse>(): Promise<ErrorResponse> {
-		return await this.#res.json();
+	readonly status;
+	readonly payload;
+
+	constructor(message: string, status: number, payload?: ErrorResponse) {
+		super(message);
+		this.status = status;
+		this.payload = payload;
 	}
 }
 
-export const fetchData = async <Data>(url: string, options?: RequestInit): Promise<Data> => {
+export const fetchData = async <Data, Error = unknown>(
+	url: string,
+	options?: RequestInit,
+): Promise<Data> => {
 	const res = await fetch(url, options);
-	if (!res.ok) throw new FetchError(res);
+	if (!res.ok) throw await FetchError.from<Error>(res);
 
 	const result = await res.json();
 	return result;
@@ -29,13 +40,16 @@ export const fetchData = async <Data>(url: string, options?: RequestInit): Promi
 
 export type Fetcher = typeof fetchData;
 
-export const fetchInternalData = async <Data>(endpoint: string, options?: RequestInit) => {
+export const fetchInternalData = async <Data, Error = unknown>(
+	endpoint: string,
+	options?: RequestInit,
+) => {
 	type InternalData = {
 		data: Data;
 	};
 
 	const url = `${env.VITE_API_URL}/${endpoint}`;
-	const { data } = await fetchData<InternalData>(url, options);
+	const { data } = await fetchData<InternalData, Error>(url, options);
 
 	return data;
 };
