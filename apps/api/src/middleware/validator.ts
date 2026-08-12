0cf678d508922595e7de99f72caa4967644e2255
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-12 13:36:55 -0300

    refactor(api): use `ApiError`

--- apps/api/src/middleware/validator.ts
+++ apps/api/src/middleware/validator.ts
@@ -3,13 +3,12 @@ import type * as z from 'zod';
 
 import { flattenErrors, sValidator } from '@hono/standard-validator';
 
+import { ApiError } from '#utils/errors.ts';
+
 export const validate = <Target extends keyof ValidationTargets, Schema extends z.ZodType>(
 	target: Target,
 	schema: Schema,
 ) =>
-	sValidator(target, schema, (result, c) => {
-		if (!result.success) {
-			const flattenedErrors = flattenErrors(result.error);
-			return c.json(flattenedErrors, 400);
-		}
+	sValidator(target, schema, (result) => {
+		if (!result.success) throw new ApiError(400, { cause: flattenErrors(result.error) });
 	});
--- apps/api/src/routes/users/services.ts
+++ apps/api/src/routes/users/services.ts
@@ -1,20 +1,20 @@
 import type { NewUser } from '@repo/contracts/users';
 
-import { HTTPException } from 'hono/http-exception';
+import { ApiError } from '#utils/errors.ts';
 
 import { compare, hash } from './crypto.ts';
 import { create, findByUsername, update } from './repository.ts';
 
 export const signUp = async ({ username, password, bestTime }: NewUser) => {
 	const user = await findByUsername(username);
-	if (user != null) throw new HTTPException(409, { message: 'Username is already taken' });
+	if (user != null) throw new ApiError(409, { message: 'Username is already taken' });
 
 	const hashedPassword = await hash(password);
 	return await create({ username, password: hashedPassword, bestTime });
 };
 
 export const signIn = async ({ username, password, bestTime }: NewUser) => {
-	const unauthorizedError = new HTTPException(401, { message: 'Incorrect credentials' });
+	const unauthorizedError = new ApiError(401, { message: 'Invalid credentials' });
 
 	const user = await findByUsername(username);
 	if (user == null) throw unauthorizedError;
