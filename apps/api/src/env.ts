36299d0ab231a73565e57af8da33fd8e295b3d9d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-12 13:36:33 -0300

    refactor(api): use `catchNodeError`

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -3,12 +3,12 @@ import process from 'node:process';
 import * as z from 'zod';
 
 import { createEnv } from '@repo/env';
-import { catchErrnoException } from '@repo/errors';
+import { catchNodeError } from '@repo/errors/node';
 
 try {
 	process.loadEnvFile();
 } catch (error) {
-	const caught = catchErrnoException(error);
+	const caught = catchNodeError(error);
 	if (caught.code !== 'ENOENT') throw caught;
 }
 
