e57db8a8539d2c14473a5e86fe451537f259350d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-12 13:37:09 -0300

    feat(api): customize error handler

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
+import { HTTPException } from 'hono/http-exception';
 import { logger } from 'hono/logger';
 
 import { env } from './env.ts';
@@ -12,3 +13,17 @@ app.use(logger());
 app.use(cors({ origin: env.CLIENT_URL }));
 
 app.route('/api/v1', routes);
+
+app.notFound((c) => {
+	const status = 404;
+	return c.json({ error: { status, message: 'Not Found' } }, status);
+});
+
+app.onError((error, c) => {
+	if (error instanceof HTTPException) return c.json({ error }, error.status);
+
+	const status = 500;
+
+	console.error(error);
+	return c.json({ error: { status, message: 'Internal Server Error' } }, status);
+});
