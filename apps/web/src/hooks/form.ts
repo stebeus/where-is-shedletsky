7420f5c22ab293c0e1cf7f20a851d5cff69e7b4d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-14 11:09:46 -0300

    refactor(web)!: simplify form hook

--- apps/web/src/hooks/form.ts
+++ apps/web/src/hooks/form.ts
@@ -2,7 +2,7 @@ import { type SubmitEvent, useState } from 'react';
 
 import { catchError } from '@repo/errors';
 
-import { FetchError, type Fetcher, fetchData, fetchInternalData } from '#utils/index.ts';
+import { FetchError, fetchData } from '#utils/fetch.ts';
 
 type HttpMethod =
 	| 'GET'
@@ -16,76 +16,52 @@ type HttpMethod =
 	| 'PATCH';
 
 type FormOptions = Partial<{
-	fetcher: Fetcher;
 	metadata: Record<string, unknown>;
 	method: HttpMethod;
 }>;
 
 type FieldErrors = [string, ...string[]];
 
-type FormErrorResponse = Readonly<{
+type FormErrorCause = {
 	formErrors: string[];
 	fieldErrors: Record<string, FieldErrors>;
-}>;
-
-export type FormError = FetchError<FormErrorResponse>;
-
-type Form = Readonly<{
-	error?: Error | FormError;
-	submit: (event: SubmitEvent) => Promise<void>;
-}>;
-
-type FormHook = (url: string, onAction: () => void, options?: FormOptions) => Form;
-
-export const getFormErrors = (error: Error | FormError) => {
-	console.log(error);
-
-	const isFormError = (error: unknown): error is FormError =>
-		FetchError.isFetchError(error) && error.status === 400;
-
-	const getFieldError = ([message]: FieldErrors) => message;
-
-	return isFormError(error) && error.payload?.fieldErrors != null
-		? Object.values(error.payload.fieldErrors).map(getFieldError)
-		: [error.message];
 };
 
-const isHtmlFormElement = (target: EventTarget) => target instanceof HTMLFormElement;
+type FormError = FetchError<FormErrorCause>;
 
-export const useForm: FormHook = (
-	url,
-	onAction,
-	{ fetcher = fetchData, metadata, method = 'POST' } = {},
+const isFormError = (value: unknown) => FetchError.isFetchError<FormErrorCause>(value);
+
+const getFieldError = ([message]: FieldErrors) => message;
+
+export const getFormErrors = (error: Error | FormError) =>
+	isFormError(error) && error.cause != null
+		? Object.values(error.cause.fieldErrors).map(getFieldError)
+		: [error.message];
+
+export const useForm = (
+	endpoint: string,
+	onAction: () => void,
+	{ metadata, method = 'POST' }: FormOptions = {},
 ) => {
 	const [error, setError] = useState<Error | FormError>();
 
-	const submit = async (event: SubmitEvent) => {
+	const submit = async (event: SubmitEvent<HTMLFormElement>) => {
 		event.preventDefault();
 
 		const { target } = event;
-		if (target == null || !isHtmlFormElement(target)) return;
 
-		const formData = new FormData(target);
-		const entries = Object.fromEntries(formData);
-		const newEntries = { ...entries, ...metadata };
+		const formData = Object.fromEntries(new FormData(target));
+		const body = JSON.stringify({ ...formData, ...metadata });
 
 		try {
-			await fetcher(url, {
-				method,
-				headers: { 'content-type': 'application/json' },
-				body: JSON.stringify(newEntries),
-			});
-
+			await fetchData(endpoint, { method, headers: { 'content-type': 'application/json' }, body });
 			target.reset();
 			onAction();
 		} catch (error) {
-			const caught = FetchError.isFetchError(error) ? error : catchError(error);
+			const caught = isFormError(error) ? error : catchError(error);
 			setError(caught);
 		}
 	};
 
 	return { error, submit } as const;
 };
-
-export const useInternalForm: FormHook = (url, onAction, options) =>
-	useForm(url, onAction, { fetcher: fetchInternalData, ...options });
