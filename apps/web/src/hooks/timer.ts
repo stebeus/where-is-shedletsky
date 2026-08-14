41fe2bc16506cc917dbfb8ba305b1a5dd9653466
stebeus <189894010+stebeus@users.noreply.github.com>
2026-08-14 11:10:00 -0300

    feat(web): extend timer hook

--- apps/web/src/hooks/timer.ts
+++ apps/web/src/hooks/timer.ts
@@ -2,7 +2,9 @@ import { useEffect, useRef } from 'react';
 
 type TimeSetter = typeof setInterval | typeof setTimeout;
 
-export const useTimer = (timeSetter: TimeSetter, handler: () => void, timeout?: number) => {
+type TimerHook = (handler: () => void, timeout?: number) => void;
+
+const useTimer = (timeSetter: TimeSetter, handler: () => void, timeout?: number) => {
 	const handlerRef = useRef(handler);
 
 	useEffect(() => {
@@ -12,9 +14,13 @@ export const useTimer = (timeSetter: TimeSetter, handler: () => void, timeout?: 
 	useEffect(() => {
 		if (timeout == null) return;
 
-		const tick = () => handlerRef.current();
-		const id = timeSetter(tick, timeout);
+		const id = timeSetter(() => handlerRef.current(), timeout);
+		const clear = timeSetter === setInterval ? clearInterval : clearTimeout;
 
-		return () => clearInterval(id);
+		return () => clear(id);
 	}, [timeout, timeSetter]);
 };
+
+export const useInterval: TimerHook = (handler, timeout) => useTimer(setInterval, handler, timeout);
+
+export const useTimeout: TimerHook = (handler, timeout) => useTimer(setTimeout, handler, timeout);
